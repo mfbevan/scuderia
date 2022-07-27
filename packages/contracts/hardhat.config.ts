291512c183fff5687b75cb0b370248187bf39389
mfbevan <matthewbevan00@gmail.com>
2022-07-27 22:22:48 +1000

    fix compiler version

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -27,7 +27,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.11",
+    version: "0.8.15",
     settings: {
       optimizer: {
         enabled: true,
