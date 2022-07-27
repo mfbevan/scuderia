e16e114ed1152d42cb10e8b69d5c8a5cf81af2e1
matthew-labrys <101532004+matthew-labrys@users.noreply.github.com>
2022-07-27 21:26:18 +1000

    init new repo

---
+++ packages/contracts/hardhat.config.ts
@@ -0,0 +1,67 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "hardhat-deploy";
+import "solidity-coverage";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-ethers";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.11",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: { sources: "contracts/" },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: 0,
+    alice: 1,
+    bob: 2,
+    carol: 3,
+    ted: 4,
+  },
+};
+
+export default config;
---
+++ packages/lib/config/index.ts
@@ -0,0 +1,7 @@
+
+const config = {
+  maxSupply: 100,
+  mintPrice: 0.1, // 0.1 ETH
+};
+
+export { config };
