b6f2a58ca5ae2e15603e87c0734c1aeb6b72db20
mfbevan <matthewbevan00@gmail.com>
2022-09-10 20:18:48 +1000

    🔧  config: point to mumbai contracts

--- packages/contracts/deployments/index.ts
+++ packages/contracts/deployments/index.ts
@@ -4,6 +4,8 @@ import { Network } from "@scuderia/lib/types";
 import { Scuderia, Scoot } from "../typechain";
 import ScuderiaLocalhost from "../deployments/localhost/Scuderia.json";
 import ScootLocalhost from "../deployments/localhost/Scoot.json";
+import ScuderiaMumbai from "../deployments/mumbai/Scuderia.json";
+import ScootMumbai from "../deployments/mumbai/Scoot.json";
 
 const network = process.env.NEXT_PUBLIC_NETWORK as Network;
 if (!network) {
@@ -17,7 +19,7 @@ interface NetworkContracts {
 
 const getContracts: Record<Network, NetworkContracts> = {
   [Network.Polygon]: { scuderia: ScuderiaLocalhost, scoot: ScootLocalhost },
-  [Network.Mumbai]: { scuderia: ScuderiaLocalhost, scoot: ScootLocalhost },
+  [Network.Mumbai]: { scuderia: ScuderiaMumbai, scoot: ScootMumbai },
   [Network.Localhost]: { scuderia: ScuderiaLocalhost, scoot: ScootLocalhost },
 };
 
