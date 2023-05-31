a0cbd390d8efbf99ae8a2fb60f8c143bdbeab99b
mfbevan <matthewbevan00@gmail.com>
2023-05-31 21:37:49 +1000

    refactor: 🔥 remove previous app

--- apps/webapp/constants/toast.ts
+++
@@ -1,9 +0,0 @@
-import { UseToastOptions } from "@chakra-ui/react";
-
-export const successToast: UseToastOptions = {
-  status: "success"
-}
-
-export const failureToast: UseToastOptions = {
-  status: "error"
-}
